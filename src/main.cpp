1e58b14d80269770237d6471bd2712ea400c425b
Wraitheend <gshillam@hotmail.com>
2026-09-23 12:29:25 -0700

    im crying i hate coding im too dumb for this

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,24 @@
 #include <string>
 #include <gtkmm.h>
 
+// Tree model columns:
+class ModelColumns : public Gtk::TreeModel::ColumnRecord
+{
+    public:
+    ModelColumns()
+    {
+        add(collumn_id);
+        add(collumn_name);
+        add(collumn_number);
+        add(collumn_percentage);
+    }
+
+    Gtk::TreeModelColumn<unsigned int>  collumn_id;
+    Gtk::TreeModelColumn<Glib::ustring> collumn_name;
+    Gtk::TreeModelColumn<short>         collumn_number;
+    Gtk::TreeModelColumn<int>           collumn_percentage;
+};
+
 class Window : public Gtk::Window
 {
   public:
@@ -14,37 +32,23 @@ class Window : public Gtk::Window
     // Signal handlers:
     void onNotebookSwitchPage(Gtk::Widget *page, guint page_num);
 
-    // Tree model columns:
-    class ModelColumns : public Gtk::TreeModel::ColumnRecord
-    {
-      public:
-        ModelColumns()
-        {
-            add(m_col_id);
-            add(m_col_name);
-            add(m_col_number);
-            add(m_col_percentage);
-        }
-
-        Gtk::TreeModelColumn<unsigned int>  m_col_id;
-        Gtk::TreeModelColumn<Glib::ustring> m_col_name;
-        Gtk::TreeModelColumn<short>         m_col_number;
-        Gtk::TreeModelColumn<int>           m_col_percentage;
-    };
-
-    ModelColumns m_Columns;
-
-    // Child widgets:
-    Gtk::Box queue_tab_box;
-
-    Gtk::ScrolledWindow          m_ScrolledWindow;
-    Gtk::TreeView                m_TreeView;
-    Glib::RefPtr<Gtk::ListStore> m_refTreeModel;
+    ModelColumns queue_tree_collumns;
+    ModelColumns album_tree_collumns;
+    ModelColumns playlist_tree_collumns;
 
     // Child widgets:
     Gtk::Notebook tabbar_notebook;
 
+    // Queue Child widgets:
+    Gtk::Box queue_tab_box;
+    Gtk::ScrolledWindow          queue_scrollable_window;
+    Gtk::TreeView                queue_tree;
+    Glib::RefPtr<Gtk::ListStore> queue_tree_refrence;
+    
+    // Album Child widgets:
     Gtk::Label album_label, playlist_label;
+
+    // Playlist Child widgets:
 };
 
 Window::Window() // : queue_tab_box(Gtk::Orientation::VERTICAL), album_label("Contents of album tab"), playlist_label("Contents of playlist tab")
@@ -53,54 +57,53 @@ Window::Window() // : queue_tab_box(Gtk::Orientation::VERTICAL), album_label("Co
     set_default_size(1920, 1080);
     set_child(tabbar_notebook);
 
-    // set_child(queue_tab_box);
-
     // Add the TreeView, inside a ScrolledWindow, with the button underneath:
-    m_ScrolledWindow.set_child(m_TreeView);
+    // queue_scrollable_window.set_margin_top(5);
+    queue_scrollable_window.set_child(queue_tree);
 
     // Only show the scrollbars when they are necessary:
-    m_ScrolledWindow.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
-    m_ScrolledWindow.set_expand();
+    queue_scrollable_window.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+    queue_scrollable_window.set_expand();
 
-    queue_tab_box.append(m_ScrolledWindow);
+    queue_tab_box.append(queue_scrollable_window);
 
     // Create the Tree model:
-    m_refTreeModel = Gtk::ListStore::create(m_Columns);
-    m_TreeView.set_model(m_refTreeModel);
+    queue_tree_refrence = Gtk::ListStore::create(queue_tree_collumns);
+    queue_tree.set_model(queue_tree_refrence);
 
     // Fill the TreeView's model
-    auto row                        = *(m_refTreeModel->append());
-    row[m_Columns.m_col_id]         = 1;
-    row[m_Columns.m_col_name]       = "Billy Bob";
-    row[m_Columns.m_col_number]     = 10;
-    row[m_Columns.m_col_percentage] = 15;
+    auto row                        = *(queue_tree_refrence->append());
+    row[queue_tree_collumns.collumn_id]         = 1;
+    row[queue_tree_collumns.collumn_name]       = "Billy Bob";
+    row[queue_tree_collumns.collumn_number]     = 10;
+    row[queue_tree_collumns.collumn_percentage] = 15;
 
-    row                             = *(m_refTreeModel->append());
-    row[m_Columns.m_col_id]         = 2;
-    row[m_Columns.m_col_name]       = "Joey Jojo";
-    row[m_Columns.m_col_number]     = 20;
-    row[m_Columns.m_col_percentage] = 40;
+    row                             = *(queue_tree_refrence->append());
+    row[queue_tree_collumns.collumn_id]         = 2;
+    row[queue_tree_collumns.collumn_name]       = "Joey Jojo";
+    row[queue_tree_collumns.collumn_number]     = 20;
+    row[queue_tree_collumns.collumn_percentage] = 40;
 
-    row                             = *(m_refTreeModel->append());
-    row[m_Columns.m_col_id]         = 3;
-    row[m_Columns.m_col_name]       = "Rob McRoberts";
-    row[m_Columns.m_col_number]     = 30;
-    row[m_Columns.m_col_percentage] = 70;
+    row                             = *(queue_tree_refrence->append());
+    row[queue_tree_collumns.collumn_id]         = 3;
+    row[queue_tree_collumns.collumn_name]       = "Rob McRoberts";
+    row[queue_tree_collumns.collumn_number]     = 30;
+    row[queue_tree_collumns.collumn_percentage] = 70;
 
     // Add the TreeView's view columns:
     // This number will be shown with the default numeric formatting.
-    m_TreeView.append_column("Title", m_Columns.m_col_id);
-    m_TreeView.append_column("Album", m_Columns.m_col_name);
+    queue_tree.append_column("Title", queue_tree_collumns.collumn_id);
+    queue_tree.append_column("Album", queue_tree_collumns.collumn_name);
 
-    m_TreeView.append_column_numeric("Artist", m_Columns.m_col_number, "%010d" /* 10 digits, using leading zeroes. */);
+    queue_tree.append_column_numeric("Artist", queue_tree_collumns.collumn_number, "%010d" /* 10 digits, using leading zeroes. */);
 
     // Display a progress bar instead of a decimal number:
     auto cell       = Gtk::make_managed<Gtk::CellRendererProgress>();
-    int  cols_count = m_TreeView.append_column("Duration", *cell);
-    auto pColumn    = m_TreeView.get_column(cols_count - 1);
+    int  cols_count = queue_tree.append_column("Duration", *cell);
+    auto pColumn    = queue_tree.get_column(cols_count - 1);
     if (pColumn)
     {
-        pColumn->add_attribute(cell->property_value(), m_Columns.m_col_percentage);
+        pColumn->add_attribute(cell->property_value(), queue_tree_collumns.collumn_percentage);
     }
 
     // Make all the columns reorderable:
@@ -109,7 +112,7 @@ Window::Window() // : queue_tab_box(Gtk::Orientation::VERTICAL), album_label("Co
     // finely control column drag and drop.
     for (guint i = 0; i < 2; i++)
     {
-        auto column = m_TreeView.get_column(i);
+        auto column = queue_tree.get_column(i);
         column->set_reorderable();
     }
 
