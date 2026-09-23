9d51b62d455f19f6d621a55012230cfd9c536663
Wraitheend <gshillam@hotmail.com>
2026-09-22 22:43:36 -0700

    adding spdx header

--- scripts/sync_main.sh
+++ scripts/sync_main.sh
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0
+# SPDX-FileCopyrightText: 2026 silver_gray
 #!/bin/bash
 
 git switch main &&
--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0
+# SPDX-FileCopyrightText: 2026 silver_gray
 #!/bin/bash
 
 clang-format -i $(find src -name '*.cpp') $(find src -name '*.h') &&
